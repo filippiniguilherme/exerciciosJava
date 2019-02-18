edb3e96380aeafb67cdfb72ab7b751e6698a3eae
filippiniguilherme <41347887+filippiniguilherme@users.noreply.github.com>
2019-02-18 09:19:25 -0300

    Add files via upload

---
+++ cursojava/src/arraysecolections/Fila.java
@@ -0,0 +1,27 @@
+package arraysecolections;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Fila {
+	
+	public static void main(String[] args) {
+		Queue<String> fila = new LinkedList<>();
+		
+		fila.offer("Maria");
+		fila.offer("João");
+		fila.offer("Pedro");
+		
+		System.out.println("Peek/Element...");
+		System.out.println(fila.peek());
+		System.out.println(fila.element());
+		
+		System.out.println("Poll/Remove");
+		System.out.println(fila.poll());
+		System.out.println(fila.remove());
+		System.out.println(fila.poll());
+		System.out.println(fila.poll());
+		
+		
+	}
+}
---
+++ cursojava/src/arraysecolections/WrapperClasses.java
@@ -0,0 +1,16 @@
+package arraysecolections;
+
+public class WrapperClasses {
+public static void main(String[] args) {
+		
+		int x = 20;
+		
+		Integer obj = x;
+		
+		System.out.println(obj);
+		
+		int y =  obj * 2;
+		
+		System.out.println(y);
+	}
+}
